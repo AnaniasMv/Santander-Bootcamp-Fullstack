f950e4e8c00646047c1b014040024b207717b630
AnaniasMv <ananiasmatheus22@outlook.com>
2022-06-09 23:02:28 -0300

    operador

--- JavaScript/sintaxe-e-operadores/numberComparator.js
+++ JavaScript/sintaxe-e-operadores/numberComparator.js
@@ -1,6 +1,6 @@
 
 function comparator(a, b) {
-    (a == b) ? console.log('os numeros ' + a + ' e ' + b + ' sao iguais') : console.log('os numeros ' + a + ' e ' + b + ' não são iguais');
+    (a === b) ? console.log('os numeros ' + a + ' e ' + b + ' sao iguais') : console.log('os numeros ' + a + ' e ' + b + ' não são iguais');
     var soma = a + b;
     if (soma > 10 && soma < 20) {
         console.log('Sua soma é de ' + soma + ' que é maior que 10 e menor que 20');
