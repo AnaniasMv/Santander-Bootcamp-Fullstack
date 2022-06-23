b8807318f3e45207887829848402f05c35f2ab3b
AnaniasMv <ananiasmatheus22@outlook.com>
2022-06-23 11:06:39 -0300

    modulos

---
+++ JavaScript/modulos/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade('Matheus', 19));
+console.log(mostraCidade('Matheus', 'São Carlos'));
+console.log(mostraHobby('Matheus', 'Dormir'));
